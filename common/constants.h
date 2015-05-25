a02a1f2ef790264b79ab864e9cb3bf4e465a2443
dmartinez31 <daniel.martinez7107@gmail.com>
2015-05-24 20:26:48 -0400

    Test to see if I can push to branch
    File Monitor first push

--- common/constants.h
+++ common/constants.h
@@ -2,3 +2,7 @@
 #define MAX_FILE_NUM 1000
 #define FILE_NAME_MAX_LEN 127
 #define MAX_PEER_NUM 10
+
+
+//File Monitor
+#define MONITOR_POLL_INTERVAL 1
---
+++ fileMonitor/fileMonitorTestClient.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <signal.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <string.h>
+#include <errno.h>
+#include <time.h>
+#include <pthread.h>
+#include <signal.h>
+#include <sys/wait.h>
+#include <netdb.h>
+#include <netinet/in.h>
+#include <sys/time.h>
+#include <assert.h>
+// #include "tracker.h"
+#include "../common/pkt.h"
+#include "../common/filetable.h"
+#include "../common/peertable.h"
+#include "../common/utils.h"
+
+void printAdd(char* name) {
+	printf("File %s added\n", name);
+}
+
+void printModify(char* name) {
+	printf("File %s modified\n", name);
+}
+
+void printDelete(char* name) {
+	printf("File %s deleted\n", name);
+}
+
+
+
+int main(int argc, char *argv[]) {
+
+struct localFileAlerts myFuncs {
+	printAdd
+	printModify
+	printDelete
+};
+
+
+pthread_t monitorthread;
+pthread_create(&monitorthread, NULL, fileMonitorThread, (void*) myFuncs);
+
+sleep(60);
+
+pthread_destroy(monitorthread);
+
+}
